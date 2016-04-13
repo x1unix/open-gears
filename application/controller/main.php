26a839fa972d7e8e18a5b707ee9708b1913c9d7e
Denis Sedchenko <ascii@live.ru>
2016-04-13 12:41:43 +0300

    Update tutorial

--- application/controller/main.php
+++ application/controller/main.php
@@ -1,17 +1,12 @@
 <?php
- System::GetModel("helper");
-
 class MainController extends Controller
 {
   // Main entry point of each controller
   public function Main()
   {
-    session_start();
     $this->SetView("index","main");
     $this->Data['title'] = "OpenGears Framework";
     $this->Data['description'] = "An lightweight PHP MVC framework";
-    $this->Data['cdate'] = TimeStamp::ToString(time());
-
     return $this->Execute();
   }
 
